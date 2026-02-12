33a253797191130e624d73e018f38a2b2d800272
Adam <a.niewczas01@gmail.com>
2026-02-12 15:45:41 +0100

    Init commit

---
+++ TicketReceiver/src/main/java/com/helpdesk/ticketreceiver/TicketReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk.ticketreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketReceiverApplication.class, args);
+    }
+
+}
