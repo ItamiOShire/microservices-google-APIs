6123153b48bd19d33a40ddc04717068409f00ba6
Adam <a.niewczas01@gmail.com>
2026-02-13 17:23:52 +0100

    add: ticket dto data from user model

---
+++ TicketReceiver/src/main/java/com/helpdesk/ticketreceiver/dto/TicketDto.java
@@ -0,0 +1,16 @@
+package com.helpdesk.ticketreceiver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TicketDto {
+
+    private String subject;
+
+    private String email;
+
+    private String message;
+
+}
