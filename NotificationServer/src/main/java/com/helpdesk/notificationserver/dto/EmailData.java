ab19921caea3b2638ee07c1d09591990526ff285
Adam <a.niewczas01@gmail.com>
2026-02-16 17:56:11 +0100

    feat: dto for emailData, such as from/to email, subject and content

---
+++ NotificationServer/src/main/java/com/helpdesk/notificationserver/dto/EmailData.java
@@ -0,0 +1,18 @@
+package com.helpdesk.notificationserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class EmailData {
+
+    private final String fromAddressEmail = "cindefoftwoworldsflames.gm@gmail.com";
+    private final String subject = "Helpdesk - info email";
+    private final String content = """
+            
+            Hello. We would like to inform you, that your issue has been reported in our system.
+            
+            """;
+    @Setter
+    private String toAddressEmail;
+}
