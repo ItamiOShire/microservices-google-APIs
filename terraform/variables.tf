3ec87ffda9db840863d2adf4c9d496bc85a2f956
Adam <a.niewczas01@gmail.com>
2026-02-18 12:57:28 +0100

    ref: update variables.tf to be sensitive and ephemeral - not to be stored in state

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,28 @@
-variable "project-id" {}
+variable "project-id" {
+  type = string
+}
 variable "region" {}
 variable "zone" {}
-variable "ticket-topic" {}
+variable "ticket-topic" {
+  sensitive = true
+}
 variable "notification-sub" {}
 variable "ticket-process-sub" {}
-variable "sa-id" {}
+variable "sa-id" {
+  sensitive = true
+  ephemeral = true
+}
 variable "ticket-receiver-image-name" {}
 variable "notification-server-image-name" {}
-variable "client-id" {}
-variable "client-secret" {}
-variable "refresh-token" {}
+variable "client-id" {
+  sensitive = true
+  ephemeral = true
+}
+variable "client-secret" {
+  sensitive = true
+  ephemeral = true
+}
+variable "refresh-token" {
+  sensitive = true
+  ephemeral = true
+}
