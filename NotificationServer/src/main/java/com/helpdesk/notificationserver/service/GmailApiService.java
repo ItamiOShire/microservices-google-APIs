3c7c493871ec6d3e587a7e021d3de02d80993a2a
Adam <a.niewczas01@gmail.com>
2026-02-19 13:59:12 +0100

    ref: inject GoogleApiConnectionService and establish connection to google client api only when it is needed (in function)

--- NotificationServer/src/main/java/com/helpdesk/notificationserver/service/GmailApiService.java
+++ NotificationServer/src/main/java/com/helpdesk/notificationserver/service/GmailApiService.java
@@ -1,43 +1,27 @@
 package com.helpdesk.notificationserver.service;
 
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
-import com.google.auth.http.HttpCredentialsAdapter;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.helpdesk.notificationserver.dto.EmailData;
-import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 @Slf4j
 public class GmailApiService {
 
-    private final GoogleCredentials credentials;
-    private final Gmail gmail;
     private final GmailEmailMessagesService messagesService;
-
+    private final GoogleApiConnectionService googleApiConnectionService;
 
     @Autowired
     public GmailApiService(
-            GoogleCredentials credentials,
-            GmailEmailMessagesService messagesService
-    ) throws Exception {
-
+            GmailEmailMessagesService messagesService,
+            GoogleApiConnectionService googleApiConnectionService
+    ) {
         this.messagesService = messagesService;
-        this.credentials = credentials;
-        this.gmail = new Gmail.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                GsonFactory.getDefaultInstance(),
-                new HttpCredentialsAdapter(this.credentials)
-        ).setApplicationName("notification-service-helpdesk").build();
-
+        this.googleApiConnectionService = googleApiConnectionService;
     }
 
     public void sendEmail(
@@ -46,6 +30,7 @@ public class GmailApiService {
 
         try {
             Message message = messagesService.createAndEncodeEmail(emailData);
+            Gmail gmail = googleApiConnectionService.getConnectionToGmail();
             Message sentMessage = gmail.users().messages().send(emailData.getFromAddressEmail(), message).execute();
 
             log.info("Sent message id: {}", sentMessage.getId());
