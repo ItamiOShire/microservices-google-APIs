19b8f979390d4403301d85ba50cd36574b269925
Adam <a.niewczas01@gmail.com>
2026-02-19 11:23:26 +0100

    fix: remove ephemeral from variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,19 +10,15 @@ variable "notification-sub" {}
 variable "ticket-process-sub" {}
 variable "sa-id" {
   sensitive = true
-  ephemeral = true
 }
 variable "ticket-receiver-image-name" {}
 variable "notification-server-image-name" {}
 variable "client-id" {
   sensitive = true
-  ephemeral = true
 }
 variable "client-secret" {
   sensitive = true
-  ephemeral = true
 }
 variable "refresh-token" {
   sensitive = true
-  ephemeral = true
 }
