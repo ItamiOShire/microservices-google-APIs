3d6fda93bca96b1a8b578842307c9ac8eb0013e3
Adam <a.niewczas01@gmail.com>
2026-02-22 17:55:59 +0100

    chore: add more meaningful logs to processing ticket method

--- TicketReceiver/src/main/java/com/helpdesk/ticketreceiver/service/TicketService.java
+++ TicketReceiver/src/main/java/com/helpdesk/ticketreceiver/service/TicketService.java
@@ -8,6 +8,7 @@ import com.helpdesk.ticketreceiver.repository.TicketRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -17,13 +18,17 @@ public class TicketService {
 
     private final TicketRepository ticketRepository;
     private final PubSubTemplate pubSubTemplate;
-    private static final String TOPIC_NAME = System.getenv("TICKET-TOPIC-NAME");
+    private final String TOPIC_NAME;
     private static final Logger LOGGER = LoggerFactory.getLogger(TicketService.class);
 
     @Autowired
-    TicketService(TicketRepository ticketRepository, PubSubTemplate pubSubTemplate) {
+    TicketService(
+            TicketRepository ticketRepository,
+            PubSubTemplate pubSubTemplate,
+            @Value("${cloud.gcp.pubsub.topic.name}") String topicName)  {
         this.ticketRepository = ticketRepository;
         this.pubSubTemplate = pubSubTemplate;
+        this.TOPIC_NAME = topicName;
     }
 
     public Mono<Ticket> processTicketRequest(TicketDto userTicket) {
@@ -31,10 +36,14 @@ public class TicketService {
         Ticket ticket = new Ticket(userTicket);
 
         Mono<Ticket> savedTicket = ticketRepository.save(ticket)
+                .doOnError(err -> LOGGER.error("Error przy zapisywaniu ticketu: {}", err.getMessage()))
                 .doOnSuccess( t -> LOGGER.info("Zapisano ticket o id {}", t.getId()))
                 .flatMap( t -> Mono.fromFuture(pubSubTemplate.publish(TOPIC_NAME, t.getEmail()))
+                        .doOnError(err -> LOGGER.error("Error przy publikowaniu wiadomości: {}" , err.getMessage()))
+                        .doOnSuccess( mess -> LOGGER.info("Opublikowano temat z wiadomością - {}", mess))
                         .thenReturn(t))
-                .doOnSuccess( t -> LOGGER.info("Opublikowano temat z wiadomością - {}", t.getEmail()));
+                .doOnError(err -> LOGGER.error("Błąd przy przetwarzaniu tematu: {}", err.getMessage()))
+                .doOnSuccess(t -> LOGGER.info("Operacja przetwarzania ticketa zakończona pomyślnie"));
 
         return savedTicket;
     }
